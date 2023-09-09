f995820b1df05b480dd497f83ca741458902ef03
postive-vibes-2023 <47699892+positive-vibes-2023@users.noreply.github.com>
2023-09-09 17:01:40 +0100

    Remove a single element form an array
    Need; While in JS a single element can be removed by using shift or pop
    methods, However both these method allow us to remove a element only
    form a specific location- pop removes the last element , while shift
    removes the first element.
    This function allows us to remove an element from any random location.
    the first commit addresses, te most simple situation, where a single
    element needs to be removed from an array which contains this element
    only once.
    We are calling the element to be removed as garbage.
    
    a period

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,32 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, garbage) {
+	garbageIndex = array.indexOf(garbage);
+	// Create a empty distalList that will capture all the popped out elements that come after the garbage.
+	// once the garbage element is popped out, the remaining array is simply
+	// concatenated with the distaList.
+	// However, we need to reverse the distalList before concatenating as it has
+	// been reversed as the popped elements were added to it from end.
 
+	distalList = [];
+	proximalList = [];
+	//
+	let i = array.length;
+
+	while (i > 0) {
+		//console.log(i);
+		poppedELement = array.pop();
+		// console.log(poppedELement);
+
+		if (poppedELement != garbage) {
+			distalList.push(poppedELement);
+			// console.log(distalList);
+		} else {
+			// console.log(array);
+			// console.log(array.concat(distalList.reverse()));
+			return array.concat(distalList.reverse());
+		}
+		i = i - 1;
+	}
 };
-
+removeFromArray([1, 2, 6, 8, 9, 4], 2);
 // Do not edit below this line
 module.exports = removeFromArray;
