3be1aacc11026e12f892a93e8733354f8419119d
postive-vibes-2023 <47699892+positive-vibes-2023@users.noreply.github.com>
2023-09-10 00:28:40 +0100

    feat(Function to check if given year is a leap year): Check if a year is leap year
    First check if divisble by 4. If not divisble by 4, then straight away
    declare not a leap year. But if divisible by 4 then first check if also
    divisible ny 400 (then it is indeed a leap year), however, if 4
    divisible year is not divsible by 400, it can be still bea leap year
    only if it is also not divisble by 100.

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,27 @@
-const leapYears = function() {
-
+const leapYears = function (year) {
+	//First check if year is  divisible by four
+	if (year % 4 == 0) {
+		// then check if divisible by 400
+		if (year % 400 == 0) {
+			// if yes >> declare true
+			console.log(`${year} is a Leap Year!`);
+			return true;
+		}
+		//if no then check if divisible by 100.
+		else if (year % 100 == 0) {
+			// Divisible by 4 $ since not divisible 400 and  divisible by 100 - declare not a leap year.
+			console.log(`${year} is a NOT a Leap Year!`);
+			return false;
+		} else {
+			//Divisible by 4 BUT but not by 400 and 100.
+			console.log(`${year} is a Leap Year!`);
+			return true;
+		}
+	} else {
+		//Since not divisible by 4- cannot be leap year.
+		console.log(`${year} is a NOT a Leap Year!`);
+		return false;
+	}
 };
 
 // Do not edit below this line
