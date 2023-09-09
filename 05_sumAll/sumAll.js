7974af11532feee17656d9272739c462a970acab
postive-vibes-2023 <47699892+positive-vibes-2023@users.noreply.github.com>
2023-09-09 23:49:41 +0100

    refactor(Checking if validity of passed arguements): Refactor the checking of the arguements
    Use array.every method to check every element for a specific condition,
    in this case, check if these are postive and are indeed numbers;The loop
    to sum the all the numbers in the range runs only after this initial
    checking. its wortjwhile to note that foreach method wont work, instead
    every method is needed to check each elment in a list for specific
    conditions.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,25 @@
-const sumAll = function() {
+const sumAll = function (start, end) {
+	list = [start, end];
+	if (list.every((element) => element >= 0 && typeof element === 'number')) {
+		// start >= 0 &&
+		// end >= 0 &&
+		// typeof start == 'number' &&
+		// typeof end == 'number'
 
+		list.sort((a, b) => a - b);
+		let val = list[0];
+		var sum = list[0];
+		while (val < list[1]) {
+			val++;
+			sum = sum + val;
+		}
+
+		return sum;
+	} else {
+		return 'ERROR';
+	}
 };
+sumAll(10, '90');
 
 // Do not edit below this line
 module.exports = sumAll;
