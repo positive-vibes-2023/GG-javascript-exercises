f6ec16db4b09ef6aa2e59d678038343728f5c887
postive-vibes-2023 <47699892+positive-vibes-2023@users.noreply.github.com>
2023-09-09 15:56:26 +0100

    clean the committed
    deleted several consol.log outputs and comments removed.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,24 +1,9 @@
-//The function requires an arguement- we wil call it word
 const reverseString = function (word) {
-	//the function will reverse the provided word
-	//use split method to convert string into a array containing each element.
 	const wordList = word.split('');
-	console.log(wordList);
-	//use reverse method to reverse the list
 	const reverseWordList = wordList.reverse();
-	console.log(reverseWordList);
-	//use join method to create a string from reversed list
 	const reversedWord = reverseWordList.join('');
-
-	// let wordList = [];
-	// let i = 0;
-	// while (i < word.length) {
-	// 	wordList.push(word[i]);
-	// 	i++;
-	// }
-	console.log(reversedWord);
-	return wordList.reverse().join('');
+	return reversedWord;
 };
-reverseString('!hello world!');
+
 // Do not edit below this line
 module.exports = reverseString;
