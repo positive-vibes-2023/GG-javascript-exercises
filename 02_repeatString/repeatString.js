06592e08a6c63d3f5a797ae04ba3ac8477a35aec
postive-vibes-2023 <gaurav0133@gmail.com>
2023-09-09 11:56:35 +0100

    Join a given string a number of times
    The function will join the user provided string n number of times where
    n is the number provided by the user too.
    THe function ensures that it works even if blank string is provided.
    The function throws an error message if a mischievous user provides
    negative value to the number of times strings need to be multiplied.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function (string, num) {
+	var stringList = [];
+	if (num < 0) {
+		return 'ERROR';
+	}
+	while (num > 0) {
+		stringList.push(string);
+		num = num - 1;
+	}
+	const finalString = stringList.join('');
+	return finalString;
 };
 
 // Do not edit below this line
