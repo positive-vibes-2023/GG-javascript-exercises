32d64c588e43d8b64beb126d7be882e7b2c20f65
postive-vibes-2023 <47699892+positive-vibes-2023@users.noreply.github.com>
2023-09-09 18:10:02 +0100

    feat(Add more features): Add capacity to remove more then one element
    Added another lopp whihc allowed removing more then one element.
    
    Closes Succefully passed secnd test in TDD.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,31 +1,42 @@
-const removeFromArray = function (array, garbage) {
-	garbageIndex = array.indexOf(garbage);
+const removeFromArray = function (array, ...args) {
 	// Create a empty distalList that will capture all the popped out elements that come after the garbage.
 	// once the garbage element is popped out, the remaining array is simply
 	// concatenated with the distaList.
 	// However, we need to reverse the distalList before concatenating as it has
 	// been reversed as the popped elements were added to it from end.
 
-	distalList = [];
-	proximalList = [];
+	garbageList = args;
+	garbageIndex = 0;
+	while (garbageIndex < garbageList.length) {
+		garbage = garbageList[garbageIndex];
 
-	let i = array.length;
-	while (i > 0) {
-		//console.log(i);
-		poppedELement = array.pop();
-		// console.log(poppedELement);
+		// find the location of item to be removed (garbage) in the main array.
+		// Store this value in var index.This will be used to find the number of
+		// times the while loop need to run.
+		index = array.indexOf(garbage);
+		requiredIterations = array.length - index;
 
-		if (poppedELement != garbage) {
-			distalList.push(poppedELement);
-			// console.log(distalList);
-		} else {
-			// console.log(array);
-			// console.log(array.concat(distalList.reverse()));
-			return array.concat(distalList.reverse());
+		// initiate a counter i and increment it till it equals
+		// required iterataions. Also reset and intialize a temp list that
+		// stores popped elements.
+		let i = 1;
+		distalList = [];
+
+		while (i <= requiredIterations) {
+			poppedELement = array.pop();
+
+			if (poppedELement != garbage) {
+				distalList.push(poppedELement);
+			} else {
+				array = array.concat(distalList.reverse());
+			}
+			i = i + 1;
 		}
-		i = i - 1;
+		garbageIndex++;
 	}
+	console.log(array);
+	return array;
 };
-removeFromArray([1, 2, 6, 8, 9, 4], 2);
+removeFromArray([1, 2, 6, 8, 9, 4], 2, 6);
 // Do not edit below this line
 module.exports = removeFromArray;
