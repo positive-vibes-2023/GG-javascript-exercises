d97f9144f8d62666602c09442369fe40de689e7e
postive-vibes-2023 <47699892+positive-vibes-2023@users.noreply.github.com>
2023-09-09 15:12:47 +0100

    Function reverse a give string
    Added a parameter to function which represent the string to be reversed
    Used while loop to read through the string and keep pushing each letter
    in string into a empty array / list (can do better- will try in next
    commit).
    
    period

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
-
+//The function requires an arguement- we wil call it word
+const reverseString = function (word) {
+	//the function will reverse the provided word
+	let wordList = [];
+	let i = 0;
+	while (i < word.length) {
+		wordList.push(word[i]);
+		i++;
+	}
+	return wordList.reverse().join('');
 };
 
 // Do not edit below this line
