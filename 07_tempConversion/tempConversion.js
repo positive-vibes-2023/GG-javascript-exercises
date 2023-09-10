406e52d76ba78600283b48eb7814454f45594169
postive-vibes-2023 <47699892+positive-vibes-2023@users.noreply.github.com>
2023-09-10 12:52:08 +0100

    feat(Whole file): Conver celsisu in Fahreneit and viceversa
    Write seperate functions to inter conver celsius and Fahreneit.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,21 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (temp) {
+	convertedTemp = (temp - 32) * (5 / 9);
+	console.log(
+		`The ${temp} degree Fahrenheit is equal to ${convertedTemp} degree celsius.`
+	);
+	return parseFloat(convertedTemp.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (temp) {
+	convertedTemp = temp * (9 / 5) + 32;
+	console.log(
+		`The ${temp} degree Fahrenheit is = ${convertedTemp} degree celsius.`
+	);
+	return parseFloat(convertedTemp.toFixed(1));
 };
-
+convertToCelsius(32);
 // Do not edit below this line
 module.exports = {
-  convertToCelsius,
-  convertToFahrenheit
+	convertToCelsius,
+	convertToFahrenheit,
 };
